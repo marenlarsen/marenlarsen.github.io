cf9dad752d11a6b72e75030e0d1202f6fbccb768
Kevin Jablonowski <kjazbo@gmail.com>
2018-03-18 12:27:52 -0400

    Update file structure and convert homepage to ejs

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res) {
-    res.send("Hello");
+    res.render("home");
 });
 
 app.listen(process.env.PORT || 3000, function() {
