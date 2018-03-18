cdd3dab1d2f3150e6c1cf862e95a89ac8ffc3168
Kevin Jablonowski <kjazbo@gmail.com>
2018-03-18 11:37:59 -0400

    Initial node.js and express.js commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require("express"),
+    app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+    res.send("Hello");
+});
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server has started on port " + process.env.PORT);
+});
